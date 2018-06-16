7963e058348ce09bceb57d65fdb24c8f3a464c49
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-06-16 20:30:22 +0200

    Set start and end time to be not null

--- app/src/main/java/se/jakob/knarkklocka/data/Alarm.java
+++ app/src/main/java/se/jakob/knarkklocka/data/Alarm.java
@@ -3,23 +3,27 @@ package se.jakob.knarkklocka.data;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
 import java.util.Date;
 
 @Entity(tableName = "alarm_table")
 public class Alarm {
+
     @PrimaryKey(autoGenerate = true)
     private int id;
     @ColumnInfo(name = "alarm_state")
     private int state;
+    @NonNull
     @ColumnInfo(name = "start_time")
     private Date startTime;
+    @NonNull
     @ColumnInfo(name = "end_time")
     private Date endTime;
     @ColumnInfo(name = "number_of_snoozes")
     private int snoozes;
 
-    public Alarm(int id, int state, Date startTime, Date endTime) {
+    public Alarm(int id, int state, @NonNull Date startTime, @NonNull Date endTime) {
         this.id = id;
         this.state = state;
         this.startTime = startTime;
@@ -35,13 +39,15 @@ public class Alarm {
 
     public void setId(int id) {this.id = id;}
 
+    @NonNull
     public Date getStartTime() {return startTime;}
 
-    public void setStartTime(Date startTime) {this.startTime = startTime;}
+    public void setStartTime(@NonNull Date startTime) {this.startTime = startTime;}
 
+    @NonNull
     public Date getEndTime() {return endTime;}
 
-    public void setEndTime(Date endTime) {this.endTime = endTime;}
+    public void setEndTime(@NonNull Date endTime) {this.endTime = endTime;}
 
     public int getSnoozes() {return snoozes;}
 
