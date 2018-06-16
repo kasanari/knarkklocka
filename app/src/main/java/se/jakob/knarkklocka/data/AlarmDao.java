dc1d7a06bde5ad4f9cb41e990d9dd068d31d44bb
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-06-16 20:31:55 +0200

    created a basic DAO

---
+++ app/src/main/java/se/jakob/knarkklocka/data/AlarmDao.java
@@ -0,0 +1,36 @@
+package se.jakob.knarkklocka.data;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface AlarmDao {
+
+    @Insert
+    void insert(Alarm alarm);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateAlarm(Alarm alarm);
+
+    @Delete
+    void deleteAlarm(Alarm alarm);
+
+    @Query("DELETE FROM alarm_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM alarm_table ORDER BY end_time")
+    LiveData<List<Alarm>> loadAllAlarms();
+
+    @Query("SELECT * FROM alarm_table WHERE id = :id")
+    LiveData<Alarm> loadAlarmById(int id);
+
+    @Query("SELECT * FROM alarm_table WHERE alarm_state = :state")
+    LiveData<Alarm> loadAlarmsByState(int state);
+}
