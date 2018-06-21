58a594c18d8733bb6748f61c7e4932d54e9fb9bf
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-06-21 18:29:17 +0200

    Refactored to AlarmDatabase

---
+++ app/src/main/java/se/jakob/knarkklocka/data/AlarmDatabase.java
@@ -0,0 +1,66 @@
+package se.jakob.knarkklocka.data;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.Date;
+
+@TypeConverters(DateConverter.class)
+@Database(entities = {Alarm.class}, version = 1, exportSchema = false)
+public abstract class AlarmDatabase extends RoomDatabase {
+    private static final String LOG_TAG = AlarmDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static volatile AlarmDatabase INSTANCE; //
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback() {
+
+                @Override
+                public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+    public static AlarmDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (LOCK) {
+                if (INSTANCE == null) {
+                    Log.d(LOG_TAG, "Creating new database instance");
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AlarmDatabase.class, "alarm_database")
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public abstract AlarmDao alarmDao(); //Getter for Dao
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final AlarmDao mDao;
+
+        PopulateDbAsync(AlarmDatabase db) {
+            mDao = db.alarmDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            mDao.deleteAll();
+            Alarm word = new Alarm(1, new Date(500), new Date(500));
+            mDao.insert(word);
+            word = new Alarm(1, new Date(600), new Date(600));
+            mDao.insert(word);
+            return null;
+        }
+    }
+}
