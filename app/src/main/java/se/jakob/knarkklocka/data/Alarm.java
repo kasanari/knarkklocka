1f55aa5cfd8ec1238f8f69db2e98b98443caa56e
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-06-21 18:29:03 +0200

    Added states

--- app/src/main/java/se/jakob/knarkklocka/data/Alarm.java
+++ app/src/main/java/se/jakob/knarkklocka/data/Alarm.java
@@ -2,6 +2,7 @@ package se.jakob.knarkklocka.data;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
@@ -9,6 +10,11 @@ import java.util.Date;
 
 @Entity(tableName = "alarm_table")
 public class Alarm {
+
+    public static final int STATE_WAITING = 0;
+    public static final int STATE_DEAD = 1;
+    public static final int STATE_ACTIVE = 2;
+    public static final int STATE_SNOOZING = 3;
 
     @PrimaryKey(autoGenerate = true)
     private int id;
@@ -22,6 +28,14 @@ public class Alarm {
     private Date endTime;
     @ColumnInfo(name = "number_of_snoozes")
     private int snoozes;
+
+    @Ignore
+    public Alarm(int state, @NonNull Date startTime, @NonNull Date endTime) {
+        this.state = state;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.snoozes = 0;
+    }
 
     public Alarm(int id, int state, @NonNull Date startTime, @NonNull Date endTime) {
         this.id = id;
