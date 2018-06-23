846aa83e81cae1e2da4c9d3f6b0559481541366b
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-06-23 12:11:24 +0200

    AlarmActivity state

--- app/src/main/java/se/jakob/knarkklocka/AlarmActivity.java
+++ app/src/main/java/se/jakob/knarkklocka/AlarmActivity.java
@@ -23,68 +23,77 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
 
+import se.jakob.knarkklocka.LocalService.LocalBinder;
 import se.jakob.knarkklocka.utils.TimerUtils;
 
 public class AlarmActivity extends Activity implements View.OnClickListener {
 
+    /**
+     * Bound AlarmService
+     */
+    LocalService mService;
+
     /** Whether the AlarmService is currently bound */
-    private boolean mServiceBound;
+    private boolean mBound = false;
 
     private static final String TAG = "AlarmActivity";
 
     private Button mSnoozeButton;
     private Button mDismissButton;
-//
-//    /**
-//     * Bind AlarmService if not yet bound.
-//     */
-//    private void bindAlarmService() {
-//        if (!mServiceBound) {
-//            final Intent intent = new Intent(this, AlarmService.class);
-//            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-//            mServiceBound = true;
-//        }
-//    }
-///*
-//    /**
-//     * Unbind AlarmService if bound.
-//     */
-//    private void unbindAlarmService() {
-//        if (mServiceBound) {
-//            unbindService(mConnection);
-//            mServiceBound = false;
-//        }
-//    }
+    /**
+     * Defines callbacks for service binding, passed to bindService()
+     */
+    private ServiceConnection mConnection = new ServiceConnection() {
 
-//    private final ServiceConnection mConnection = new ServiceConnection() {
-//        @Override
-//        public void onServiceConnected(ComponentName name, IBinder service) {
-//            Log.i(TAG,"Finished binding to AlarmService");
-//        }
-//
-//        @Override
-//        public void onServiceDisconnected(ComponentName name) {
-//            Log.i(TAG,"Disconnected from AlarmService");
-//        }
-//    };
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            LocalBinder binder = (LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    /**
+     * Bind AlarmService if not yet bound.
+     */
+    private void bindAlarmService() {
+        final Intent intent = new Intent(this, AlarmService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    /**
+     * Unbind AlarmService if bound.
+     */
+    private void unbindAlarmService() {
+        unbindService(mConnection);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
-        //Klaxon.vibrateOnce(this);
 
+        /* Turn on vibration */
+        Klaxon.vibrateOnce(this);
+
+        /* Keep screen turned on */
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                 | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
                 | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
                 | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
 
+        /*Hide navigation bar*/
         hideNavigationBar();
 
         // Close dialogs and window shade, so this is fully visible
@@ -99,30 +108,47 @@ public class AlarmActivity extends Activity implements View.OnClickListener {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        //bindAlarmService();
+    protected void onStart() {
+        super.onStart();
+        final Intent intent = new Intent(this, LocalService.class);
+        boolean success = bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        //mService.startAlarm();
+        dismiss();
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        WakeLocker.releaseCpuLock();
-        //unbindAlarmService();
+    protected void onStop() {
+        super.onStop();
+        //mService.stopAlarm();
+        unbindAlarmService();
+        mBound = false;
     }
 
-    public void snooze() {
+    /*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bindAlarmService();
+    }*/
+
+    /*@Override
+    protected void onPause() {
+        super.onPause();
         //unbindAlarmService();
-        WakeLocker.releaseCpuLock();
+    }*/
+
+    public void snooze() {
         Intent startNewTimerIntent = new Intent(this, TimerIntentService.class);
         startNewTimerIntent.setAction(TimerUtils.ACTION_SNOOZE_TIMER);
         startService(startNewTimerIntent);
+        //mService.stopAlarm();
+        unbindAlarmService();
         finish();
     }
 
     public void dismiss() {
-        //unbindAlarmService();
-        WakeLocker.releaseCpuLock();
+        //mService.stopAlarm();
+        unbindAlarmService();
         finish();
     }
 
