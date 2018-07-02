65c898488234606b99353f3fb580875b9e39d529
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-07-02 20:58:11 +0200

    added ids to function calls

--- app/src/main/java/se/jakob/knarkklocka/utils/TimerUtils.java
+++ app/src/main/java/se/jakob/knarkklocka/utils/TimerUtils.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import java.util.Date;
+
 import se.jakob.knarkklocka.AlarmActivity;
 import se.jakob.knarkklocka.AlarmService;
 import se.jakob.knarkklocka.LogUtils;
@@ -21,22 +23,24 @@ public class TimerUtils {
     public static final String ACTION_SET_NEW_TIMER = "set-new-timer";
     public static final String ACTION_REMOVE_TIMER = "remove-timer";
     public static final String ACTION_SNOOZE_TIMER = "snooze-timer";
+    public static final String EXTRA_END_TIME = "end-time";
+    public static final String EXTRA_ALARM_ID = "alarm-id";
     private static final String TAG = "TimerUtils";
     private static final int ALARM_INTENT_ID = 76;          //Arbitrary unique ID for the alarm intent
     private static final int TIMER_ACTIVITY_INTENT_ID = 34; //Arbitrary unique ID for the TimerActivity intent
 
     //private static int testing_time = 5000;
 
-    public static void executeTask(Context context, String action) {
+    public static void executeTask(Context context, String action, int id) {
         switch (action) {
             case ACTION_SET_NEW_TIMER:
-                setNewAlarm(context);
+                setNewAlarm(context, id);
                 break;
             case ACTION_REMOVE_TIMER:
-                cancelAlarm(context);
+                cancelAlarm(context, id);
                 break;
             case ACTION_SNOOZE_TIMER:
-                setSnooze(context);
+                setSnooze(context, id);
                 break;
         }
     }
@@ -44,92 +48,61 @@ public class TimerUtils {
     /**
      * Returns the pending intent that starts the Alarm service
      **/
-    private static PendingIntent getAlarmServiceIntent(Context context) {
+    private static PendingIntent getAlarmServiceIntent(Context context, long id) {
         Intent alarmIntent = new Intent(context, AlarmService.class);
+        alarmIntent.putExtra(EXTRA_ALARM_ID, id);
         return PendingIntent.getService(context, ALARM_INTENT_ID, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     /**
      * Returns the pending intent that starts the Alarm service
      **/
-    private static PendingIntent getAlarmActivityIntent(Context context) {
+    private static PendingIntent getAlarmActivityIntent(Context context, long id) {
         Intent alarmIntent = new Intent(context, AlarmActivity.class);
+        alarmIntent.putExtra(EXTRA_ALARM_ID, id);
         return PendingIntent.getActivity(context, ALARM_INTENT_ID, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     /**
      * Returns the pending intent that starts the main timer activity
      **/
-    private static PendingIntent getShowAlarmIntent(Context context) {
+    private static PendingIntent getShowAlarmIntent(Context context, long id) {
         Intent timerIntent = new Intent(context, TimerActivity.class);
+        timerIntent.putExtra(EXTRA_ALARM_ID, id);
         return PendingIntent.getActivity(context, TIMER_ACTIVITY_INTENT_ID, timerIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
-    /**
-     * Fetches the upcoming alarm if there is one
-     **/
-    private static AlarmManager.AlarmClockInfo getNextAlarm(Context context) {
-        AlarmManager alarmManager = context.getSystemService(AlarmManager.class);
-        if (alarmManager != null) {                                                          //Check that we found the Alarm Manager
-            AlarmManager.AlarmClockInfo nextAlarmClock = alarmManager.getNextAlarmClock();   //Fetch the next upcoming alarm
-            if (nextAlarmClock != null) {                                                    //Check that we found an upcoming alarm
-                if (nextAlarmClock.getShowIntent().equals(getShowAlarmIntent(context))) {    //Check that the upcoming alarm is ours
-                    LogUtils.d("Found upcoming drug alarm!");
-                    return nextAlarmClock;
-                }
-            } else {
-                return null;
-            }
-        }
-        return null;
-    }
-
-    public static long getRemainingTime(Context context) {
-        AlarmManager.AlarmClockInfo nextAlarm = getNextAlarm(context);
-        if (nextAlarm != null) {
-            return nextAlarm.getTriggerTime() - System.currentTimeMillis();
-        } else {
-            return -1;
-        }
-    }
-
-    public static boolean alarmIsRunning(Context context) {
-        return getNextAlarm(context) != null;
-    }
-
-
-    public static void setNewAlarm(Context context) {
+    public static void setNewAlarm(Context context, long id) {
         long timer_duration = PreferenceUtils.getTimerLength(context);
-        setNewAlarm(context, timer_duration);
+        setNewAlarm(context, id, new Date(timer_duration));
     }
 
-    public static void setNewAlarm(Context context, long length) {
-        AlarmManager alarm = context.getSystemService(AlarmManager.class);
+    public static void setNewAlarm(Context context, long id, Date endTime) {
+        AlarmManager alarmManager = context.getSystemService(AlarmManager.class);
 
         /*Setup AlarmService to start when alarm goes off*/
-        PendingIntent pendingAlarmIntent = getAlarmActivityIntent(context);//getAlarmServiceIntent(context);
+        PendingIntent pendingAlarmIntent = getAlarmActivityIntent(context, id);//getAlarmServiceIntent(context);
 
         /*Setup alarm clock info*/
-        long wakeup_time = System.currentTimeMillis() + length;
-        PendingIntent pi = getShowAlarmIntent(context);
-        AlarmManager.AlarmClockInfo alarmInfo = new AlarmManager.AlarmClockInfo(wakeup_time, pi);
+        //long wakeup_time = System.currentTimeMillis() + length;
+        PendingIntent showAlarmPI = getShowAlarmIntent(context, id);
+        AlarmManager.AlarmClockInfo alarmInfo = new AlarmManager.AlarmClockInfo(endTime.getTime(), showAlarmPI);
 
-        if (alarm != null) {
-            alarm.setAlarmClock(alarmInfo, pendingAlarmIntent);
+        if (alarmManager != null) {
+            alarmManager.setAlarmClock(alarmInfo, pendingAlarmIntent);
             Log.d(TAG, "Set an alarm");
         }
     }
 
-    public static void setSnooze(Context context) {
+    public static void setSnooze(Context context, long id) {
         long length = PreferenceUtils.getSnoozeLength(context);
-        if (!alarmIsRunning(context)) {
-            setNewAlarm(context, length);
-        }
+        setNewAlarm(context, id, new Date(length));
     }
 
-    public static void cancelAlarm(Context context) {
+
+    public static void cancelAlarm(Context context, long id) {
         AlarmManager alarm = context.getSystemService(AlarmManager.class);
-        PendingIntent pendingAlarmIntent = getAlarmServiceIntent(context);
+        PendingIntent pendingAlarmIntent = getAlarmActivityIntent(context, id);
         if (alarm != null) {
             alarm.cancel(pendingAlarmIntent);
             LogUtils.d("Cancelled alarm");
