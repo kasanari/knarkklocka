d2d5f85247513cb5a7086048946328946b051581
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-07-08 10:13:06 +0200

    removed service binding code and added livedtaa

--- app/src/main/java/se/jakob/knarkklocka/AlarmService.java
+++ app/src/main/java/se/jakob/knarkklocka/AlarmService.java
@@ -1,64 +1,68 @@
 package se.jakob.knarkklocka;
 
-import android.app.Service;
+import android.arch.lifecycle.LifecycleService;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.content.Intent;
-import android.os.Binder;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-public class AlarmService extends Service {
+import java.util.List;
+
+import se.jakob.knarkklocka.data.Alarm;
+import se.jakob.knarkklocka.data.AlarmRepository;
+
+import static se.jakob.knarkklocka.utils.TimerUtils.EXTRA_ALARM_ID;
+
+public class AlarmService extends LifecycleService {
 
     private static final String TAG = "AlarmService";
 
-    // Binder given to clients
-    private final IBinder mBinder = new LocalBinder();
-
-    /** Whether the service is currently bound to AlarmActivity */
-    private boolean mIsBound = false;
-
     private PowerManager.WakeLock wakeLock;
+    private AlarmRepository mRepository;
+    private LiveData<List<Alarm>> currentAlarms;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d("Testing", "Service got created");
-        Intent intent = new Intent(this, AlarmActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        startAlarm();
+        mRepository = new AlarmRepository(getApplication());
+        currentAlarms = mRepository.getAllAlarms();
+        currentAlarms.observe(this, new Observer<List<Alarm>>() {
+            @Override
+            public void onChanged(@Nullable final List<Alarm> alarmList) {
+                Log.d(TAG, alarmList.toString());
+            }
+        });
     }
 
-    @Override
-    public boolean onUnbind(Intent intent) {
-        mIsBound = false;
-        return super.onUnbind(intent);
+    public void startAlarmActivity() {
+        Intent alarmActivityIntent = new Intent(this, AlarmActivity.class);
+        alarmActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(alarmActivityIntent);
     }
-/*
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG, "onStartCommand initiated");
-        //Intent alarm_activity_intent = new Intent(this, AlarmActivity.class);
-        //startActivity(alarm_activity_intent);
+        long alarmID = intent.getLongExtra(EXTRA_ALARM_ID, -1);
+        //currentAlarm.getValue().setState(Alarm.STATE_ACTIVE);
+        // AppExecutors.getInstance().diskIO().execute(new Runnable() {
+        //     @Override
+        //     public void run() {
+        //         //mRepository.update(currentAlarm.getValue());
+        //     }
+        // });
+        //  startAlarmActivity();
         return super.onStartCommand(intent, flags, startId);
     }
-*/
-    /**
-     * Class used for the client Binder.  Because we know this service always
-     * runs in the same process as its clients, we don't need to deal with IPC.
-     */
-    public class LocalBinder extends Binder {
-        AlarmService getService() {
-            // Return this instance of AlarmService so clients can call public methods
-            return AlarmService.this;
-        }
-    }
 
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        mIsBound = true;
-        return mBinder;
+        super.onBind(intent);
+        return null;
     }
 
     public void startAlarm() {
