a6de76282d011c832ed5a8cb6c6cfc935cfebb6c
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-07-21 14:25:15 +0200

    changed vibration pattern

--- app/src/main/java/se/jakob/knarkklocka/Klaxon.java
+++ app/src/main/java/se/jakob/knarkklocka/Klaxon.java
@@ -16,8 +16,9 @@ public class Klaxon {
 
     public static void vibrateAlarm(Context context) {
         Vibrator vibrator = context.getSystemService(Vibrator.class);
-        long[] mVibratePattern = new long[]{0, 500, 600, 700, 800};
-        VibrationEffect effect = VibrationEffect.createWaveform(mVibratePattern, 1);
+        long[] mVibratePattern = new long[]{1000, 1500};
+        int[] amps = new int[]{255, 0};
+        VibrationEffect effect = VibrationEffect.createWaveform(mVibratePattern, amps, 0);
         if (vibrator != null) {
             if (!vibrator.hasAmplitudeControl()) {
                 Log.d("DEBUG", "Device does not have amplitude control");
