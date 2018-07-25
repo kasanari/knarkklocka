4f92c2ac6eef2648fc3a9f17fcb06fc9ec3228e2
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-07-25 20:01:31 +0200

    created alarmviewmodel

--- app/src/main/java/se/jakob/knarkklocka/data/AlarmActivityViewModel.java
+++ app/src/main/java/se/jakob/knarkklocka/data/AlarmActivityViewModel.java
@@ -6,53 +6,9 @@ import android.arch.lifecycle.LiveData;
 
 import java.util.Date;
 
-public class AlarmActivityViewModel extends AndroidViewModel implements AlarmViewModel {
-    private LiveData<Alarm> mAlarm;
-    private AlarmRepository mRepository;
-
+public class AlarmActivityViewModel extends AlarmViewModel {
     public AlarmActivityViewModel(Application application) {
         super(application);
-        mRepository = new AlarmRepository(application);
-        this.mAlarm = mRepository.getActiveAlarm();
-    }
-
-    public LiveData<Alarm> getAlarm() {
-        return mAlarm;
-    }
-
-    public Alarm getCurrentAlarm() {
-        return mAlarm.getValue();
-    }
-
-    public long add(Alarm alarm) {
-        return mRepository.insert(alarm);
-    }
-
-    public void delete() {
-        if (mAlarm.getValue() != null) {
-            mRepository.delete(mAlarm.getValue());
-        }
-    }
-
-    public void kill() {
-        if (isAlarmRunning()) {
-            Alarm alarm = mAlarm.getValue();
-            alarm.setState(Alarm.STATE_DEAD);
-            mRepository.update(alarm);
-        }
-    }
-
-    public void snooze(Date endTime) {
-        if (mAlarm.getValue() != null) {
-            Alarm alarm = mAlarm.getValue();
-            alarm.setState(Alarm.STATE_SNOOZING);
-            alarm.incrementSnoozes();
-            alarm.setEndTime(endTime);
-            mRepository.update(alarm);
-        }
-    }
-
-    public boolean isAlarmRunning() {
-        return mAlarm.getValue() != null;
+        super.mAlarm = mRepository.getActiveAlarm();
     }
 }
--- app/src/main/java/se/jakob/knarkklocka/data/AlarmViewModel.java
+++ app/src/main/java/se/jakob/knarkklocka/data/AlarmViewModel.java
@@ -1,4 +1,58 @@
 package se.jakob.knarkklocka.data;
 
-public class AlarmViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import java.util.Date;
+
+public class AlarmViewModel extends AndroidViewModel {
+    protected LiveData<Alarm> mAlarm;
+    protected AlarmRepository mRepository;
+
+    protected AlarmViewModel(Application application) {
+        super(application);
+        mRepository = new AlarmRepository(application);
+        //this.mAlarm = mRepository.getActiveAlarm();
+    }
+
+    public LiveData<Alarm> getAlarm() {
+        return mAlarm;
+    }
+
+    public Alarm getCurrentAlarm() {
+        return mAlarm.getValue();
+    }
+
+    public long add(Alarm alarm) {
+        return mRepository.insert(alarm);
+    }
+
+    public void delete() {
+        if (mAlarm.getValue() != null) {
+            mRepository.delete(mAlarm.getValue());
+        }
+    }
+
+    public void kill() {
+        if (alarmIsRunning()) {
+            Alarm alarm = mAlarm.getValue();
+            alarm.setState(Alarm.STATE_DEAD);
+            mRepository.update(alarm);
+        }
+    }
+
+    public void snooze(Date endTime) {
+        if (mAlarm.getValue() != null) {
+            Alarm alarm = mAlarm.getValue();
+            alarm.setState(Alarm.STATE_SNOOZING);
+            alarm.incrementSnoozes();
+            alarm.setEndTime(endTime);
+            mRepository.update(alarm);
+        }
+    }
+
+    public boolean alarmIsRunning() {
+        return mAlarm.getValue() != null;
+    }
 }
