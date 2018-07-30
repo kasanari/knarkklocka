18137c2f7e2873e478b8d2c8d85aa4b51e687517
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-07-30 17:34:42 +0200

    changed title and addded icon

--- app/src/main/java/se/jakob/knarkklocka/AlarmNotificationsBuilder.java
+++ app/src/main/java/se/jakob/knarkklocka/AlarmNotificationsBuilder.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.SystemClock;
 import android.support.v4.content.ContextCompat;
@@ -22,44 +23,41 @@ import static se.jakob.knarkklocka.utils.TimerUtils.EXTRA_ALARM_ID;
 
 public class AlarmNotificationsBuilder {
 
-    private static int ALARM_NOTIFICATION_ID = 1995;
     private static final String ALARM_NOTIFICATION_CHANNEL_ID = "reminder_notification_channel";
     private static final int ALARM_FIRING_NOTIFICATION_ID = 8;
 
     public static synchronized void showNotification(Service service, Alarm alarm) {
 
-        NotificationManager notificationManager = (NotificationManager)
-                service.getSystemService(Context.NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel mChannel = new NotificationChannel(
-                    ALARM_NOTIFICATION_CHANNEL_ID,
-                    service.getString(R.string.main_notification_channel_name),
-                    NotificationManager.IMPORTANCE_HIGH);
-            mChannel.enableVibration(true);
-            mChannel.enableLights(true);
-            mChannel.setBypassDnd(true);
-            mChannel.setShowBadge(false);
-            mChannel.setLightColor(0xff0000);
-            notificationManager.createNotificationChannel(mChannel);
+        NotificationManager notificationManager = (NotificationManager) service.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel mChannel = new NotificationChannel(
+                        ALARM_NOTIFICATION_CHANNEL_ID,
+                        service.getString(R.string.main_notification_channel_name),
+                        NotificationManager.IMPORTANCE_HIGH);
+                mChannel.enableVibration(true);
+                mChannel.enableLights(true);
+                mChannel.setBypassDnd(true);
+                mChannel.setShowBadge(false);
+                mChannel.setLightColor(0xff0000);
+                notificationManager.createNotificationChannel(mChannel);
+            }
         }
 
 
         Notification.Builder notification = new Notification.Builder(service, ALARM_NOTIFICATION_CHANNEL_ID)
-                .setContentTitle("Alarm Title!")
-                .setContentText("The is an alarm!")
-                .setColor(ContextCompat.getColor(service, R.color.colorPrimary))
-                .setSmallIcon(R.drawable.ic_pill)
+                .setContentTitle("THE DRUG ALARM IS GOING OFF!")
+                .setContentText("Time for drugs!")
+                .setColor(ContextCompat.getColor(service, R.color.colorAccent))
+                .setSmallIcon(R.drawable.ic_alarm_white_24dp)
+                .setLargeIcon(BitmapFactory.decodeResource(service.getResources(), R.mipmap.pill))
                 .setOngoing(true)
                 .setAutoCancel(false)
                 .setCategory(Notification.CATEGORY_ALARM)
                 .setVisibility(Notification.VISIBILITY_PUBLIC)
                 .setLocalOnly(true);
 
-        final Date base = alarm.getEndTime();
-
-        // Setup Content Action
-        //PendingIntent contentIntent = TimerUtils.getShowAlarmIntent(context, alarm.getId());
-        //notification.setContentIntent(contentIntent);
+        final Date base = alarm.getEndTime(); // For the Chronometer
 
         // Full screen intent has flags so it is different than the content intent.
         final Intent fullScreen = new Intent(service, AlarmActivity.class)
@@ -69,6 +67,7 @@ public class AlarmNotificationsBuilder {
         final PendingIntent pendingFullScreen = PendingIntent.getActivity(service, 0, fullScreen, FLAG_UPDATE_CURRENT);
         notification.setFullScreenIntent(pendingFullScreen, true);
 
+        // Setup Content Intent
         notification.setContentIntent(pendingFullScreen);
 
         service.startForeground(ALARM_FIRING_NOTIFICATION_ID, notification.build());
