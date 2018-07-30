830cfc1ecb6e13380bdf8b966653ffaf27c7a2f0
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-07-30 17:42:40 +0200

    alarmservice handles intents

--- app/src/main/java/se/jakob/knarkklocka/AlarmService.java
+++ app/src/main/java/se/jakob/knarkklocka/AlarmService.java
@@ -1,83 +1,96 @@
 package se.jakob.knarkklocka;
 
 import android.arch.lifecycle.LifecycleService;
-import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.Observer;
-import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
-import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 
 import se.jakob.knarkklocka.data.Alarm;
 import se.jakob.knarkklocka.data.AlarmRepository;
 
+import static se.jakob.knarkklocka.data.Alarm.STATE_ACTIVE;
+import static se.jakob.knarkklocka.utils.TimerUtils.ACTION_ACTIVATE_ALARM;
+import static se.jakob.knarkklocka.utils.TimerUtils.ACTION_STOP_ALARM;
 import static se.jakob.knarkklocka.utils.TimerUtils.EXTRA_ALARM_ID;
 
 public class AlarmService extends LifecycleService {
 
     private static final String TAG = "AlarmService";
-
-    private PowerManager.WakeLock wakeLock;
     private AlarmRepository mRepository;
-    private LiveData<Alarm> currentAlarm;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("Testing", "Service got created");
+        Log.d(TAG, "Service got created");
         mRepository = new AlarmRepository(getApplication());
-    }
-
-    public void startAlarmActivity() {
-        Intent alarmActivityIntent = new Intent(this, AlarmActivity.class);
-        alarmActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(alarmActivityIntent);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        //PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-        //wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE, TAG);
-        //wakeLock.acquire(30000);
-        WakeLocker.acquire(this);
-        long alarmID = intent.getLongExtra(EXTRA_ALARM_ID, -1);
-        currentAlarm = mRepository.getAlarmByID(alarmID);
-        currentAlarm.observe(this, new Observer<Alarm>() {
-            @Override
-            public void onChanged(@Nullable final Alarm alarm) {
-                if (alarm != null) {
+        final long id = intent.getLongExtra(EXTRA_ALARM_ID, -1);
+        final String action = intent.getAction();
+        if (action != null ) {
+            AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                @Override
+                public void run() {
+                    handleIntent(action, id);
+                }
+            });
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    /*Intent handler meant to be run on separate thread*/
+    public void handleIntent(String action, long id) {
+        Alarm alarm = mRepository.getAlarmByID(id);
+            switch (action) {
+                case ACTION_ACTIVATE_ALARM:
                     if (BuildConfig.DEBUG) {
                         DateFormat df = DateFormat.getTimeInstance(DateFormat.SHORT);
-                        String debugString = String.format(Locale.getDefault(), "Activating alarm with id %d due %s", alarm.getId(), df.format(alarm.getEndTime()));
+                        String debugString = String.format(Locale.getDefault(), "Activating alarm with id %d due %s", id, df.format(alarm.getEndTime()));
                         Log.d(TAG, debugString);
                     }
 
-                    if (alarm.getSnoozes() > 10) {
+                    if (alarm.getSnoozes() < 10) {
+                        mRepository.changeAlarmState(id, STATE_ACTIVE);
+                        startAlarm(alarm);
+                    } else {
                         alarm.setState(Alarm.STATE_DEAD);
                         mRepository.update(alarm);
-                        WakeLocker.release();
+                        stopAlarm();
                         stopSelf();
-                    } else {
-                        AppExecutors.getInstance().diskIO().execute(new Runnable() {
-                            @Override
-                            public void run() {
-                                alarm.setState(Alarm.STATE_ACTIVE);
-                                mRepository.update(alarm);
-                                WakeLocker.release();
-                                stopSelf();
-                            }
-                        });
-                        startAlarmActivity();
                     }
-                }
+                    break;
+
+                case ACTION_STOP_ALARM:
+                    stopAlarm();
+                    stopSelf();
+                    break;
             }
-        });
-        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void startAlarm(Alarm alarm) {
+        WakeLocker.acquire(this);
+        Klaxon.vibrateAlarm(this);
+        AlarmNotificationsBuilder.showNotification(this, alarm);
+    }
+
+    private void stopAlarm() {
+        Klaxon.stopVibrate(this);
+        stopForeground(true);
+        WakeLocker.release();
+    }
+
+    @Override
+    public void onDestroy() {
+        LogUtils.v("AlarmService.onDestroy() called");
+        super.onDestroy();
+        stopAlarm();
     }
 
     @Nullable
