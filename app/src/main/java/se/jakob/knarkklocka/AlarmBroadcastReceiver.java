0bbe42cb33b95a335da30d14a66038b8c0512cfc
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-07-30 18:05:58 +0200

    added null guard

--- app/src/main/java/se/jakob/knarkklocka/AlarmBroadcastReceiver.java
+++ app/src/main/java/se/jakob/knarkklocka/AlarmBroadcastReceiver.java
@@ -15,14 +15,17 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
         Log.d(TAG, "Broadcast Receiver started");
         // We're creating a new intent that's going to start the AlarmActivity
         PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE, TAG);
-        wakeLock.acquire(30000);
-        Intent in = new Intent(context, AlarmActivity.class);
-        // You need to add this to your intent if you want to start an Activity fromm a class
-        // that is not an Activity itself
-        in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        // Now we just start the Activity
-        context.startActivity(in);
-        wakeLock.release();
+        PowerManager.WakeLock wakeLock;
+        if (pm != null) {
+            wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE, TAG);
+            wakeLock.acquire(30000);
+            Intent in = new Intent(context, AlarmActivity.class);
+            // You need to add this to your intent if you want to start an Activity fromm a class
+            // that is not an Activity itself
+            in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            // Now we just start the Activity
+            context.startActivity(in);
+            wakeLock.release();
+        }
     }
 }
