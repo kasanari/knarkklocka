1efe7992a88168930dad24adc241d9d9a0a2567a
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-08-26 15:06:34 +0200

    Added Constants class

---
+++ app/src/main/java/se/jakob/knarkklocka/utils/Constants.kt
@@ -0,0 +1,8 @@
+package se.jakob.knarkklocka.utils
+
+/**
+ * Constants used throughout the app.
+ */
+const val ACTION_ACTIVATE_ALARM = "activate-alarm"
+const val ACTION_STOP_ALARM = "stop-alarm"
+const val ACTION_WAITING_ALARM = "wating-alarm"
