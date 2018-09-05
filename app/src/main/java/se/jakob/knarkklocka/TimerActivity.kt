b9a5087e991c72d8fc1c82dab6d51889fd4ef0f9
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-09-05 18:12:24 +0200

    moved comment

--- app/src/main/java/se/jakob/knarkklocka/TimerActivity.kt
+++ app/src/main/java/se/jakob/knarkklocka/TimerActivity.kt
@@ -158,8 +158,7 @@ class TimerActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_timer, menu)
+        menuInflater.inflate(R.menu.menu_timer, menu) // Inflate the menu; this adds items to the action bar if it is present.
         return true
     }
 
