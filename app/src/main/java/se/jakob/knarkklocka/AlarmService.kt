68fea972c8ac5e5772834cb8516aec7d3237fb62
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-09-06 16:09:39 +0200

    removed wakelocker

--- app/src/main/java/se/jakob/knarkklocka/AlarmService.kt
+++ app/src/main/java/se/jakob/knarkklocka/AlarmService.kt
@@ -85,7 +85,6 @@ class AlarmService : LifecycleService() {
 
     private fun startAlarm(alarm: Alarm) {
         AlarmNotificationsUtils.clearAllNotifications(this)
-        //WakeLocker.acquire(this)
         Klaxon.vibrateAlarm(this)
         AlarmNotificationsUtils.showActiveAlarmNotification(this, alarm)
     }
@@ -93,7 +92,6 @@ class AlarmService : LifecycleService() {
     private fun stopAlarm() {
         Klaxon.stopVibrate(this)
         stopForeground(true)
-        //WakeLocker.release()
     }
 
     override fun onDestroy() {
