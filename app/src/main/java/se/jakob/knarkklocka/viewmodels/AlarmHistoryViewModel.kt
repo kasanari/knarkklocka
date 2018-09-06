f199831fe64250c37173b62250e261646c658c19
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-09-06 16:18:00 +0200

    changed var name

--- app/src/main/java/se/jakob/knarkklocka/viewmodels/AlarmHistoryViewModel.kt
+++ app/src/main/java/se/jakob/knarkklocka/viewmodels/AlarmHistoryViewModel.kt
@@ -1,14 +1,17 @@
 package se.jakob.knarkklocka.viewmodels
 
+import android.arch.lifecycle.LiveData
+import se.jakob.knarkklocka.data.Alarm
 import se.jakob.knarkklocka.data.AlarmRepository
 
 class AlarmHistoryViewModel internal constructor(private val repository: AlarmRepository) : AlarmViewModel(repository) {
 
     val allAlarms = repository.getAllAlarms()
-    override var alarm = repository.currentAlarm
+    override var liveAlarm = repository.currentAlarm
 
     fun clearHistory() {
         repository.deleteAll()
     }
 
+    fun getAlarms() = allAlarms
 }
