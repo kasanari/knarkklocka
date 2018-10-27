8514ed4918cadbb4bc7bc188ab6f6418629c60bf
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-10-27 14:16:30 +0200

    created alarmservicetest

---
+++ app/src/androidTest/java/se/jakob/knarkklocka/AlarmServiceTest.kt
@@ -0,0 +1,62 @@
+package se.jakob.knarkklocka
+
+import android.content.Context
+import android.os.IBinder
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.rule.ServiceTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import se.jakob.knarkklocka.data.AlarmRepository
+import se.jakob.knarkklocka.data.AlarmState
+import se.jakob.knarkklocka.utilities.createTestAlarm
+import se.jakob.knarkklocka.utilities.getValue
+import se.jakob.knarkklocka.utilities.testSnoozeLength
+import se.jakob.knarkklocka.utilities.testTimerLength
+import se.jakob.knarkklocka.utils.InjectorUtils
+import se.jakob.knarkklocka.utils.PreferenceUtils
+import se.jakob.knarkklocka.utils.TimerUtils
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class AlarmServiceTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val serviceRule = ServiceTestRule()
+
+    lateinit var repository: AlarmRepository
+
+    @Before
+    fun waitForABit() { // To let the system settle
+        Thread.sleep(1000)
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        repository = InjectorUtils.getAlarmRepository(context)
+        repository.deleteAll()
+        PreferenceUtils.setMainTimerLength(context, testTimerLength)
+        PreferenceUtils.setSnoozeTimerLength(context, testSnoozeLength)
+    }
+
+    @Test
+    fun startServiceTest() {
+        var alarm = createTestAlarm(Calendar.getInstance(), testTimerLength)
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        var id = repository.insert(alarm)
+        Thread.sleep(2000)
+
+        val intent = TimerUtils.getAlarmServiceIntent(context, id)
+
+        Thread.sleep(2000)
+        alarm = getValue(repository.getLiveAlarmByID(id))
+        serviceRule.startService(intent)
+        Thread.sleep(2000)
+        assertEquals(AlarmState.STATE_ACTIVE, alarm.state)
+    }
+
+}
