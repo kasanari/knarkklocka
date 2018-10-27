8b46271e822620cd6b2d905c15b1df7b165337c7
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-10-27 14:17:28 +0200

    added binding functionality

--- app/src/main/java/se/jakob/knarkklocka/AlarmService.kt
+++ app/src/main/java/se/jakob/knarkklocka/AlarmService.kt
@@ -4,13 +4,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleService
 import se.jakob.knarkklocka.data.Alarm
 import se.jakob.knarkklocka.data.AlarmRepository
-import se.jakob.knarkklocka.data.AlarmState
 import se.jakob.knarkklocka.utils.*
 import se.jakob.knarkklocka.utils.TimerUtils.EXTRA_ALARM_ID
 import java.text.DateFormat
@@ -23,6 +23,8 @@ class AlarmService : LifecycleService() {
     private val mRepository: AlarmRepository =
             InjectorUtils.getAlarmRepository(this)
     private var mIsRegistered: Boolean = false
+    private val binder: IBinder = Binder()
+    private var isBound: Boolean = false
 
 
     private val mActionsReceiver = object : BroadcastReceiver() {
@@ -109,6 +111,12 @@ class AlarmService : LifecycleService() {
 
     override fun onBind(intent: Intent?): IBinder? {
         super.onBind(intent)
-        return null
+        isBound = true
+        return binder
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        isBound = false
+        return super.onUnbind(intent)
     }
 }
