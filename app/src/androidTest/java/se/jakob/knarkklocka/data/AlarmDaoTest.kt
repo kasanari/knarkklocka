c81ef72eacae9305abaee7c627a3828889916453
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-10-27 14:16:13 +0200

    updated dependencies and added some annotations

--- app/src/androidTest/java/se/jakob/knarkklocka/data/AlarmDaoTest.kt
+++ app/src/androidTest/java/se/jakob/knarkklocka/data/AlarmDaoTest.kt
@@ -1,28 +1,29 @@
 package se.jakob.knarkklocka.data
 
-
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
-import androidx.test.InstrumentationRegistry
+import org.hamcrest.CoreMatchers.equalTo
 
-import org.hamcrest.Matchers.equalTo
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.*
 
 import org.junit.Assert.assertThat
-
-import org.junit.After
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Test
+import org.junit.runner.RunWith
 
 import se.jakob.knarkklocka.utilities.getValue
-
 import java.util.*
 
+@RunWith(AndroidJUnit4::class)
 class AlarmDaoTest {
     private lateinit var database: AlarmDatabase
     private lateinit var alarmDao: AlarmDao
     private val alarmA = Alarm(1, AlarmState.STATE_DEAD, Calendar.getInstance().apply { add(Calendar.MINUTE, 4) }.time, Calendar.getInstance().apply { add(Calendar.HOUR, 4) }.time)
     private val alarmB = Alarm(2, AlarmState.STATE_WAITING, Calendar.getInstance().apply { add(Calendar.MINUTE, 5) }.time, Calendar.getInstance().apply { add(Calendar.HOUR, 4) }.time)
     private val alarmC = Alarm(3, AlarmState.STATE_SNOOZING, Calendar.getInstance().apply { add(Calendar.MINUTE, 6) }.time, Calendar.getInstance().apply { add(Calendar.HOUR, 4) }.time)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun createDb() {
