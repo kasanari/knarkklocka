55e94d86f2d32eb6b1a8760ec5b00c3df84779f5
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-11-03 10:55:17 +0100

    function to check if app if whitelisted from battery optimizations

--- app/src/main/java/se/jakob/knarkklocka/utils/Utils.kt
+++ app/src/main/java/se/jakob/knarkklocka/utils/Utils.kt
@@ -1,8 +1,15 @@
 package se.jakob.knarkklocka.utils
 
+import android.content.Context
+import android.os.PowerManager
+import android.util.Log
+import se.jakob.knarkklocka.BuildConfig
+import se.jakob.knarkklocka.TimerActivity
 import java.util.Locale
 
 object Utils {
+
+    private const val TAG = "Utils"
 
     val creditsString: String
         get() {
@@ -10,4 +17,13 @@ object Utils {
             val number = (Math.random() * emoji.size).toInt()
             return String.format(Locale.getDefault(), "Made with %s by Jakob Nyberg", emoji[number])
         }
+
+    fun checkIfWhiteListed(context: Context) {
+        val isWhiteListed = (context.getSystemService(Context.POWER_SERVICE) as PowerManager).isIgnoringBatteryOptimizations(BuildConfig.APPLICATION_ID)
+        if (isWhiteListed) {
+            Log.d(TAG, "Application is ignoring battery optimizations.")
+        } else {
+            Log.d(TAG, "Application is NOT ignoring battery optimizations.")
+        }
+    }
 }
