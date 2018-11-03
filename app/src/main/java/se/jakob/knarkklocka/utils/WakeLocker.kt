e740a991f4451008d994f0b8d040f7174072ee87
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-11-03 10:57:48 +0100

    changed how wakelocker functions

--- app/src/main/java/se/jakob/knarkklocka/utils/WakeLocker.kt
+++ app/src/main/java/se/jakob/knarkklocka/utils/WakeLocker.kt
@@ -7,23 +7,34 @@ import android.util.Log
 import se.jakob.knarkklocka.BuildConfig
 
 object WakeLocker {
-    private const val TAG = "WakeLocker"
-    private var wakeLock: PowerManager.WakeLock? = null
+    private const val TAG = ":knarkklocka:WakeLocker"
+    private lateinit var wakeLock: PowerManager.WakeLock
+    private var locked = false
 
     fun acquire(context: Context) {
-        val pm = context.getSystemService(Context.POWER_SERVICE) as PowerManager
-        wakeLock = wakeLock ?: pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP or PowerManager.ON_AFTER_RELEASE, TAG).also {
-            it.acquire(10 * MINUTE_IN_MILLIS)
-            if (BuildConfig.DEBUG) {
-                Log.d(TAG, "Acquired WakeLock")
+        if (locked) {
+            wakeLock.release()
+        }
+        wakeLock = (context.getSystemService(Context.POWER_SERVICE) as PowerManager).run {
+            newWakeLock(PowerManager.PARTIAL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP or PowerManager.ON_AFTER_RELEASE, TAG).apply {
+                acquire(10 * MINUTE_IN_MILLIS)
+                locked = true
+                if (BuildConfig.DEBUG) {
+                    Log.d(TAG, "Acquired WakeLock")
+                }
             }
         }
     }
 
     fun release() {
-        wakeLock?.run {
-            this.release()
-            wakeLock = null
+        if (locked) {
+            try {
+                wakeLock.release()
+                locked = false
+            } catch (e: Exception) {
+                //probably already released
+                Log.e(TAG, e.message)
+            }
             if (BuildConfig.DEBUG) {
                 Log.d(TAG, "Released WakeLock")
             }
