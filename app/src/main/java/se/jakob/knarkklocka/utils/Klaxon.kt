f0d8ed067d85f5297237530c3f471a0296026065
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-11-25 09:43:25 +0100

    restructure klaxon

--- app/src/main/java/se/jakob/knarkklocka/utils/Klaxon.kt
+++ app/src/main/java/se/jakob/knarkklocka/utils/Klaxon.kt
@@ -7,26 +7,28 @@ import android.util.Log
 
 object Klaxon {
     private const val TAG = "Klaxon"
+    private val mVibratePattern = longArrayOf(1000, 1500)
+    private  val amps = intArrayOf(255, 0)
+
     fun vibrateOnce(context: Context) {
         val vibe = VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE)
-        val vibrator = context.getSystemService(Vibrator::class.java)
-        vibrator?.vibrate(vibe)
+        context.getSystemService(Vibrator::class.java).run {
+            vibrate(vibe)
+        }
     }
 
     fun vibrateAlarm(context: Context) {
-        val vibrator = context.getSystemService(Vibrator::class.java)
-        val mVibratePattern = longArrayOf(1000, 1500)
-        val amps = intArrayOf(255, 0)
         val effect = VibrationEffect.createWaveform(mVibratePattern, amps, 0)
-        if (!vibrator.hasAmplitudeControl()) {
-            Log.d(TAG, "Device does not have amplitude control")
+        context.getSystemService(Vibrator::class.java).run {
+            vibrate(effect)
+            Log.d(TAG, "Vibration started")
         }
-        vibrator?.vibrate(effect)
     }
 
     fun stopVibrate(context: Context) {
-        val vibrator = context.getSystemService(Vibrator::class.java)
-        vibrator?.cancel()
+        context.getSystemService(Vibrator::class.java)?.run {
+            cancel()
+            Log.d(TAG, "Vibration stopped")
+        }
     }
-
 }
