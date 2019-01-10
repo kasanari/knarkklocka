6557b75d7f4c1dea716906c66de8c41a2b02343a
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-01-10 16:13:30 +0100

    create alarmStateChanger

---
+++ app/src/main/java/se/jakob/knarkklocka/utils/AlarmStateChanger.kt
@@ -0,0 +1,38 @@
+package se.jakob.knarkklocka.utils
+
+import se.jakob.knarkklocka.data.Alarm
+import se.jakob.knarkklocka.data.AlarmRepository
+import java.util.*
+
+object AlarmStateChanger {
+
+    suspend fun sleep(alarm: Alarm, repository: AlarmRepository) : Boolean {
+        return when {
+            alarm.active or alarm.snoozing or alarm.missed -> {
+                kill(alarm, repository)
+            }
+            alarm.waiting -> repository.safeDelete(alarm)
+            else -> false
+        }
+    }
+
+    suspend fun kill(alarm: Alarm, repository: AlarmRepository) : Boolean {
+        alarm.kill()
+        return repository.safeUpdate(alarm)
+    }
+
+    suspend fun miss(alarm: Alarm, repository: AlarmRepository) : Boolean {
+        alarm.miss()
+        return repository.safeUpdate(alarm)
+    }
+
+    suspend fun snooze(alarm: Alarm, endTime: Date, repository: AlarmRepository) : Boolean {
+        alarm.snooze(endTime)
+        return repository.safeUpdate(alarm)
+    }
+
+    suspend fun activate(alarm: Alarm, repository: AlarmRepository) : Boolean {
+        alarm.activate()
+        return repository.safeUpdate(alarm)
+    }
+}
